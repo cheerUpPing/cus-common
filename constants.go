f88819531957db4368b219d317d6662376a717f0
cheerUpPing <cheerUpPing@163.com>
2022-04-18 15:04:47 +0800

    Update constants.go

--- constants.go
+++ constants.go
@@ -2,6 +2,6 @@ package c_common
 
 const LOG_PATH = "LOG_PATH"
 const LOG_FILE_NAME_PREFIX = "zerolog"
-const LOG_TIME_FORMAT = "_%Y%m%d%H%M.log"
+const LOG_TIME_FORMAT = "_%Y%m%d.log"
 const TIME_FORMAT = "2006-01-02 15:04:05.000"
 const TRACE_ID = "trace_id"
